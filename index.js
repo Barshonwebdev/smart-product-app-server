2e9cfa01d3e9b415084c2c8ca9092b4df7391e01
Barshon <barshonweb@gmail.com>
2024-05-31 02:34:06 +0600

    mongodb insert data via post api on server

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ const client = new MongoClient(uri, {
       await client.connect();
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      const smartProductDB=client.db('smart-product-db');
+      const productCollection=smartProductDB.collection('product-collection');
+
+      app.post('/gadgets', async(req,res)=>{
+        const productData=req.body;
+        const result=await productCollection.insertOne(productData);
+        res.send(result);
+      })
     } finally {
     //   await client.close();
     }
